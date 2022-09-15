ba018cb6511c609588b0fbb022e6afc299c56249
HP <HP@LAPTOP-Q8FSUAT2>
2022-09-15 13:20:34 +0530

    First Commit

---
+++ Day-3_JPA with Inheritance/src/com/cg/joinedinheritance/Manager.java
@@ -0,0 +1,25 @@
+package com.cg.joinedinheritance;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@SuppressWarnings("unused")
+@Entity
+@Table(name="Manager2")
+public class Manager extends Employee
+{
+	private static final long serialVersionUID = 1L;
+	private String deptname;
+	
+	//getters and setters method
+	public String getDeptname() {
+		return deptname;
+	}
+	public void setDeptname(String deptname) {
+		this.deptname = deptname;
+	}
+	
+	
+
+}
